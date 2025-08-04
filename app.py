ba34cab495342f9b9bb94f8a4b412922e6aa237d
Diego Vite Hernández <undecode0@hotmail.com>
2025-08-04 04:09:35 -0600

    Add CORS support to the Flask application

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import numpy as np
 import pandas as pd
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 # Variables globales
 model = None
